8b9c08b131ac7887cbb3e0d0d2f5815e9f399ab3
sebb1x <jsebastien509@gmail.com>
2026-06-15 22:22:43 -0400

    decorators

---
+++ decorators.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+import time
+
+def only_even_seconds(func):
+
+    def wrapper(message):
+
+        current_second = datetime.now().second
+
+        if current_second % 2 != 0:
+            return
+        return func(message)
+
+    return wrapper
+
+
+@only_even_seconds
+def shout(message):
+    print(message.upper())
+
+
+shout("hello!")
+
+for _ in range(10):
+    shout("hello!")
+    time.sleep(1)
